0f932f4a00908b2c8302711b65e1b1b71cc2cfde
Sonu Ansari <786sonuansari123@gmail.com>
2026-03-18 23:07:11 +0530

    Task 2 — Company Department List System (ConfigurationProperties)

---
+++ march_18_2026_Spring-Boot_02_ConfigurationProperties_Company_Department_List_System/src/main/java/com/nit/task/Assignment.java
@@ -0,0 +1,32 @@
+/*Task 2 — Company Department List System (ConfigurationProperties)
+
+Classes Involved:
+
+Company
+
+Department
+
+CompanyRunner
+
+Company Class Fields:
+
+List<Department> departments
+
+Department Class Fields:
+
+deptId
+
+deptName
+
+Responsibility:
+
+Bind list of departments
+
+Runner Class Responsibility:
+
+Iterate and display departments
+Output:
+---- Department List ----
+Dept ID : 101 | Dept Name : HR
+Dept ID : 102 | Dept Name : IT
+Dept ID : 103 | Dept Name : Finance*/
