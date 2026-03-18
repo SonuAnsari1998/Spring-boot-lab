c05914a52f5f11b4756a45d34246da5d75d74342
Sonu Ansari <786sonuansari123@gmail.com>
2026-03-18 23:10:43 +0530

    Task 4 — Bank Account Validation System (ConfigurationProperties)

---
+++ march_18_2026_Spring-Boot_04_ConfigurationProperties_Bank_Account_Validation_System/src/main/java/com/nit/task/Assigment.java
@@ -0,0 +1,37 @@
+/*Task 4 — Bank Account Validation System (ConfigurationProperties)
+march_18_2026_Spring-Boot_04_ConfigurationProperties_Bank_Account_Validation_System
+
+Classes Involved:
+
+BankAccount
+
+BankRunner
+
+BankAccount Class Fields:
+
+accountNumber
+
+holderName
+
+balance
+
+Responsibility:
+
+Bind properties
+
+Validate balance > 0
+
+Runner Class Responsibility:
+
+Display account or error
+Valid Output:
+
+---- Bank Account Details ----
+Account No : 1234567890
+Name       : Rahman Khan
+Balance    : 5000
+
+Invalid Output:
+
+Error: Balance must be greater than 0
+*/
---
+++ march_18_2026_Spring-Boot_04_ConfigurationProperties_Bank_Account_Validation_System/src/test/java/com/nit/March182026SpringBoot04ConfigurationPropertiesBankAccountValidationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class March182026SpringBoot04ConfigurationPropertiesBankAccountValidationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
