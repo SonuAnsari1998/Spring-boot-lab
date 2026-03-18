6c5ad107c68f067db485e27c2d47b1568115b007
Sonu Ansari <786sonuansari123@gmail.com>
2026-03-18 23:13:49 +0530

    Task 6 — Order Nested Object System (ConfigurationProperties)

---
+++ march_18_2026_Spring-Boot_06_ConfigurationProperties_Order_Nested_Object_System/src/main/java/com/nit/task/Assignment.java
@@ -0,0 +1,41 @@
+/*Task 6 — Order Nested Object System (ConfigurationProperties)
+march_18_2026_Spring-Boot_06_ConfigurationProperties_Order_Nested_Object_System
+
+Classes Involved:
+
+Order
+
+Customer
+
+OrderRunner
+
+Order Class Fields:
+
+orderId
+
+Customer customer
+
+Customer Class Fields:
+
+name
+
+address
+
+Responsibility:
+
+Bind nested configuration
+
+Runner Class Responsibility:
+
+Display order + customer details
+
+Output:
+
+---- Order Details ----
+Order ID : ORD101
+
+---- Customer Details ----
+Name     : Rohit
+Address  : Hyderabad
+
+*/
---
+++ march_18_2026_Spring-Boot_06_ConfigurationProperties_Order_Nested_Object_System/src/test/java/com/nit/March182026SpringBoot06ConfigurationPropertiesOrderNestedObjectSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class March182026SpringBoot06ConfigurationPropertiesOrderNestedObjectSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
