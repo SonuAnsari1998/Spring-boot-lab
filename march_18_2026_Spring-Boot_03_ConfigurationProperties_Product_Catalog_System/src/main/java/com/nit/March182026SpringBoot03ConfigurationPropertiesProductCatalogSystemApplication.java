9106e62e05be8093d539abeec4320a2c80d574d2
Sonu Ansari <786sonuansari123@gmail.com>
2026-03-18 23:09:14 +0530

    Task 3 — Product Catalog System (ConfigurationProperties)

---
+++ march_18_2026_Spring-Boot_03_ConfigurationProperties_Product_Catalog_System/src/main/java/com/nit/March182026SpringBoot03ConfigurationPropertiesProductCatalogSystemApplication.java
@@ -0,0 +1,13 @@
+package com.nit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class March182026SpringBoot03ConfigurationPropertiesProductCatalogSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(March182026SpringBoot03ConfigurationPropertiesProductCatalogSystemApplication.class, args);
+	}
+
+}
---
+++ march_18_2026_Spring-Boot_03_ConfigurationProperties_Product_Catalog_System/src/main/java/com/nit/task/Assignment.java
@@ -0,0 +1,38 @@
+/*Task 3 — Product Catalog System (ConfigurationProperties)
+march_18_2026_Spring-Boot_03_ConfigurationProperties_Product_Catalog_System
+
+Classes Involved:
+
+Store
+
+Product
+
+StoreRunner
+
+Store Class Fields:
+
+List<Product> products
+
+Product Class Fields:
+
+id
+
+name
+
+price
+
+Responsibility:
+
+Bind product list
+
+Runner Class Responsibility:
+
+Display all products
+
+Output:
+
+---- Product List ----
+ID : 1 | Name : Laptop  | Price : 60000
+ID : 2 | Name : Mobile  | Price : 20000
+ID : 3 | Name : Tablet  | Price : 15000
+*/
