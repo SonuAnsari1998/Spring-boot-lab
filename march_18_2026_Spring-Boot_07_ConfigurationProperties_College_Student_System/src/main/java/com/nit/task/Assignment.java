b3430ffb281e250f438a5e2d9caa007ff09fa21b
Sonu Ansari <786sonuansari123@gmail.com>
2026-03-18 23:15:31 +0530

    Task 7 — College Student System (ConfigurationProperties)

---
+++ march_18_2026_Spring-Boot_07_ConfigurationProperties_College_Student_System/src/main/java/com/nit/task/Assignment.java
@@ -0,0 +1,39 @@
+/*Task 7 — College Student System (ConfigurationProperties)
+march_18_2026_Spring-Boot_07_ConfigurationProperties_College_Student_System
+
+Classes Involved:
+
+College
+
+Student
+
+CollegeRunner
+
+College Class Fields:
+
+List<Student> students
+
+Student Class Fields:
+
+id
+
+name
+
+marks
+
+Responsibility:
+
+Bind list of students
+
+Runner Class Responsibility:
+
+Display all students
+
+Output:
+
+---- Student List ----
+ID : 1 | Name : Rahman | Marks : 85
+ID : 2 | Name : Ali    | Marks : 78
+ID : 3 | Name : Sara   | Marks : 92
+
+*/
