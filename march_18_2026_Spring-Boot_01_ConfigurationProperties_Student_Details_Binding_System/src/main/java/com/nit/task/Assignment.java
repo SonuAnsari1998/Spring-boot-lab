5d6a1f6da17dcedf56b7d530f013f2538dcb07b8
Sonu Ansari <786sonuansari123@gmail.com>
2026-03-18 23:04:55 +0530

    Task 1 — Student Details Binding System (ConfigurationProperties)

---
+++ march_18_2026_Spring-Boot_01_ConfigurationProperties_Student_Details_Binding_System/src/main/java/com/nit/task/Assignment.java
@@ -0,0 +1,35 @@
+/*Task 1 — Student Details Binding System (ConfigurationProperties)
+
+Classes Involved:
+
+Student
+
+StudentRunner (Main/Runner class)
+
+Student Class Fields:
+
+id
+
+name
+
+course
+
+fee
+
+Responsibility:
+
+Bind properties using prefix student
+
+Runner Class Responsibility:
+
+Fetch Student object
+
+Display student details
+
+Output:
+
+---- Student Details ----
+ID      : 101
+Name    : Rahat
+Course  : B.Tech
+Fee     : 75000*/
