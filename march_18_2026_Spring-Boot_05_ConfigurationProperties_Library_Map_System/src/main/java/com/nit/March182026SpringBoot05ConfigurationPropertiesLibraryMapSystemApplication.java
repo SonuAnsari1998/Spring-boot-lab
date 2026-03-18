419570dfcc5d7801c5b5043eb4aa788755b58fd5
Sonu Ansari <786sonuansari123@gmail.com>
2026-03-18 23:12:15 +0530

    Task 5 — Library Map System (ConfigurationProperties)

---
+++ march_18_2026_Spring-Boot_05_ConfigurationProperties_Library_Map_System/src/main/java/com/nit/March182026SpringBoot05ConfigurationPropertiesLibraryMapSystemApplication.java
@@ -0,0 +1,13 @@
+package com.nit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class March182026SpringBoot05ConfigurationPropertiesLibraryMapSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(March182026SpringBoot05ConfigurationPropertiesLibraryMapSystemApplication.class, args);
+	}
+
+}
---
+++ march_18_2026_Spring-Boot_05_ConfigurationProperties_Library_Map_System/src/main/java/com/nit/task/Assignment.java
@@ -0,0 +1,27 @@
+/*Task 5 — Library Map System (ConfigurationProperties)
+march_18_2026_Spring-Boot_05_ConfigurationProperties_Library_Map_System
+
+Classes Involved:
+
+Library
+
+LibraryRunner
+
+Library Class Fields:
+
+Map<String, String> books
+
+Responsibility:
+
+Bind key-value pairs
+
+Runner Class Responsibility:
+
+Display all books
+
+Output:
+
+---- Library Books ----
+java   : Effective Java
+spring : Spring in Action
+python : Learning Python*/
